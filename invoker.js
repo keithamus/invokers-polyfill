f977a04b984ca85a7abeaa99ddba7965c999dc38
Keith Cirkel <keithamus@users.noreply.github.com>
2023-12-11 11:06:09 +0000

    refactor, pass WPTs

--- invoker.js
+++ invoker.js
@@ -1,273 +1,231 @@
-export function isSupported() {
-  return (
-    typeof HTMLButtonElement !== "undefined" &&
-    typeof HTMLButtonElement.prototype === "object" &&
+(() => {
+  if (
+    typeof HTMLButtonElement === "undefined" ||
+    typeof HTMLButtonElement.prototype !== "object" ||
     "invokeTargetElement" in HTMLButtonElement.prototype
-  );
-}
-
-const ShadowRoot = globalThis.ShadowRoot || function () {};
-
-class InvokeEvent extends Event {
-  constructor(invokeEventInit = {}) {
-    super("invoke", invokeEventInit);
-    this.#invoker = invokeEventInit.invoker || null;
-    this.#action = String(invokeEventInit.action) || "auto";
+  ) {
+    return;
   }
 
-  #invoker = null;
-  get invoker() {
-    return this.#invoker;
-  }
-
-  #action = "auto";
-  get action() {
-    return this.#action;
-  }
-}
-
-const invokerAssociatedElements = new WeakMap();
-
-function getRootNode(node) {
-  if (typeof node.getRootNode === "function") {
-    return node.getRootNode();
-  }
-  if (node.parentNode) return getRootNode(node.parentNode);
-  return node;
-}
-
-function applyInvokerMixin(ElementClass) {
-  Object.defineProperties(ElementClass.prototype, {
-    invokeTargetElement: {
+  function makeEnumerable(obj, key) {
+    Object.defineProperty(obj, key, {
+      ...Object.getOwnPropertyDescriptor(obj, key),
       enumerable: true,
-      configurable: true,
-      set(targetElement) {
-        if (targetElement === null) {
-          this.removeAttribute("invoketarget");
-          invokerAssociatedElements.delete(this);
-          teardownInvokeListeners(this);
-        } else if (!(targetElement instanceof Element)) {
-          throw new TypeError(`invokeTargetElement must be an element or null`);
-        } else {
-          this.setAttribute("invoketarget", "");
-          invokerAssociatedElements.set(this, targetElement);
-          setupInvokeListeners(this);
-        }
-      },
-      get() {
-        if (this.localName !== "button" && this.localName !== "input") {
-          return null;
-        }
-        if (
-          this.localName === "input" &&
-          this.type !== "reset" &&
-          this.type !== "image" &&
-          this.type !== "button"
-        ) {
-          return null;
-        }
-        if (this.disabled) {
-          return null;
-        }
-        if (this.form && this.type === "submit") {
-          return null;
-        }
-        const targetElement = invokerAssociatedElements.get(this);
-        if (targetElement) {
-          if (targetElement.isConnected) {
-            return targetElement;
-          } else {
+    });
+  }
+
+  function getRootNode(node) {
+    if (node && typeof node.getRootNode === "function") {
+      return node.getRootNode();
+    }
+    if (node && node.parentNode) return getRootNode(node.parentNode);
+    return node;
+  }
+
+  const ShadowRoot = globalThis.ShadowRoot || function () {};
+
+  const invokeEventInvokers = new WeakMap();
+  const invokeEventActions = new WeakMap();
+
+  class InvokeEvent extends Event {
+    constructor(type, invokeEventInit = {}) {
+      super(type, invokeEventInit);
+      const { invoker, action } = invokeEventInit;
+      if (invoker != null && !(invoker instanceof Element)) {
+        throw new TypeError(`invoker must be an element`);
+      }
+      invokeEventInvokers.set(this, invoker || null);
+      invokeEventActions.set(
+        this,
+        action !== undefined ? String(action) : "auto",
+      );
+    }
+
+    get [Symbol.toStringTag]() {
+      return "InvokeEvent";
+    }
+
+    get invoker() {
+      if (!invokeEventInvokers.has(this)) {
+        throw new TypeError("illegal invocation");
+      }
+      const invoker = invokeEventInvokers.get(this);
+      if (!(invoker instanceof Element)) return null;
+      const invokerRoot = getRootNode(invoker);
+      if (invokerRoot !== getRootNode(this.target || document)) {
+        return invokerRoot.host;
+      }
+      return invoker;
+    }
+
+    get action() {
+      if (!invokeEventActions.has(this)) {
+        throw new TypeError("illegal invocation");
+      }
+      return invokeEventActions.get(this);
+    }
+  }
+  makeEnumerable(InvokeEvent.prototype, "invoker");
+  makeEnumerable(InvokeEvent.prototype, "action");
+
+  const invokerAssociatedElements = new WeakMap();
+
+  function applyInvokerMixin(ElementClass) {
+    Object.defineProperties(ElementClass.prototype, {
+      invokeTargetElement: {
+        enumerable: true,
+        configurable: true,
+        set(targetElement) {
+          if (targetElement === null) {
+            this.removeAttribute("invoketarget");
             invokerAssociatedElements.delete(this);
+            teardownInvokeListeners(this);
+          } else if (!(targetElement instanceof Element)) {
+            throw new TypeError(
+              `invokeTargetElement must be an element or null`,
+            );
+          } else {
+            this.setAttribute("invoketarget", "");
+            const targetRootNode = getRootNode(targetElement);
+            const thisRootNode = getRootNode(this);
+            if (
+              thisRootNode === targetRootNode ||
+              targetRootNode === this.ownerDocument
+            ) {
+              invokerAssociatedElements.set(this, targetElement);
+              setupInvokeListeners(this);
+            } else {
+              invokerAssociatedElements.delete(this);
+              teardownInvokeListeners(this);
+            }
+          }
+        },
+        get() {
+          if (this.localName !== "button" && this.localName !== "input") {
             return null;
           }
-        }
-        const root = getRootNode(this);
-        const idref = this.getAttribute("invoketarget");
-        if ((root instanceof Document || root instanceof ShadowRoot) && idref) {
-          return root.getElementById(idref) || null;
-        }
-        return null;
+          if (
+            this.localName === "input" &&
+            this.type !== "reset" &&
+            this.type !== "image" &&
+            this.type !== "button"
+          ) {
+            return null;
+          }
+          if (this.disabled) {
+            return null;
+          }
+          if (this.form && this.type === "submit") {
+            return null;
+          }
+          const targetElement = invokerAssociatedElements.get(this);
+          if (targetElement) {
+            if (targetElement.isConnected) {
+              return targetElement;
+            } else {
+              invokerAssociatedElements.delete(this);
+              return null;
+            }
+          }
+          const root = getRootNode(this);
+          const idref = this.getAttribute("invoketarget");
+          if (
+            (root instanceof Document || root instanceof ShadowRoot) &&
+            idref
+          ) {
+            return root.getElementById(idref) || null;
+          }
+          return null;
+        },
       },
-    },
-    invokerAction: {
-      enumerable: true,
-      configurable: true,
-      get() {
-        const value = (this.getAttribute("invokeaction") || "").toLowerCase();
-        if (value) return value;
-        return "auto";
+      invokeAction: {
+        enumerable: true,
+        configurable: true,
+        get() {
+          const value = this.getAttribute("invokeaction") || "";
+          if (value) return value;
+          return "auto";
+        },
+        set(value) {
+          this.setAttribute("invokeaction", value);
+        },
       },
-      set(value) {
-        this.setAttribute("invokeaction", value);
-      },
-    },
-  });
-}
-
-function handleInvokerActivation(event) {
-  if (!event.isTrusted) return;
-  if (event.defaultPrevented) return;
-  const invoker = event.target;
-
-  if (!invoker.invokeTargetElement) {
-    return teardownInvokeListeners(invoker);
+    });
   }
 
-  switch (event.type) {
-    case "click": {
-      return handleDefaultInvoke(invoker);
+  function handleInvokerActivation(event) {
+    if (event.defaultPrevented) return;
+    const invoker = event.target;
+
+    if (!invoker.invokeTargetElement) {
+      return teardownInvokeListeners(invoker);
+    }
+
+    if (event.type === "click") {
+      const invokee = invoker.invokeTargetElement;
+      const event = new InvokeEvent("invoke", {
+        action: invoker.invokerAction,
+        invoker,
+      });
+      invokee.dispatchEvent(event);
+      if (event.defaultPrevented) return;
+
+      const action = event.action.toLowerCase();
+
+      if (invokee.popover) {
+        const canShow = !invokee.matches(":popover-open");
+        const shouldShow =
+          canShow &&
+          (action === "auto" ||
+            action === "togglepopover" ||
+            action === "showpopover");
+        const shouldHide =
+          !canShow && (action === "auto" || action === "hidepopover");
+
+        if (shouldShow) {
+          invokee.showPopover();
+        } else if (shouldHide) {
+          invokee.hidePopover();
+        }
+      } else if (invokee.localName === "dialog") {
+        const canShow = !invokee.hasAttribute("open");
+        const shouldShow =
+          canShow && (action === "auto" || action === "showmodal");
+        const shouldHide =
+          !canShow && (action === "auto" || action === "close");
+
+        if (shouldShow) {
+          invokee.showModal();
+        } else if (shouldHide) {
+          invokee.close();
+        }
+      }
     }
   }
-}
 
-const lastVolumes = new WeakMap();
-function handleDefaultInvoke(invoker) {
-  const invokee = invoker.invokeTargetElement;
-  const event = new InvokeEvent({
-    action: invoker.invokerAction,
-    invoker,
-  });
-  invokee.dispatchEvent(event);
-  if (event.defaultPrevented) return;
-
-  if (event.action === "requestfullscreen") {
-    event.target.requestFullscreen();
-    return;
-  } else if (event.action === "exitfullscreen") {
-    document.exitFullscreen();
-    return;
-  } else if (event.action === "togglefullscreen") {
-    if (document.fullscreenElement) {
-      document.exitFullscreen();
-    } else {
-      event.target.requestFullscreen();
-    }
-    return;
+  function setupInvokeListeners(target) {
+    target.addEventListener("click", handleInvokerActivation);
   }
 
-  switch (invokee.localName) {
-    case "input": {
-      if (event.action === "auto" || event.action === "showpicker") {
-        event.target.showPicker();
-      }
-      break;
-    }
-    case "select": {
-      if (event.action === "auto" || event.action === "showpicker") {
-        event.target.showPicker?.();
-      }
-      break;
-    }
-
-    case "details": {
-      switch (event.action) {
-        case "auto": {
-          if (event.target.hasAttribute("open")) {
-            event.target.removeAttribute("open");
-          } else {
-            event.target.setAttribute("open", "");
-          }
-          break;
-        }
-        case "close": {
-          if (event.target.hasAttribute("open")) {
-            event.target.removeAttribute("open");
-          }
-          break;
-        }
-        case "open": {
-          if (!event.target.hasAttribute("open")) {
-            event.target.setAttribute("open", "");
-          }
-          break;
-        }
-      }
-      break;
-    }
-
-    case "dialog": {
-      switch (event.action) {
-        case "auto": {
-          if (event.target.hasAttribute("open")) {
-            event.target.close();
-          } else {
-            event.target.showModal();
-          }
-          break;
-        }
-        case "showModal": {
-          if (!event.target.hasAttribute("open")) {
-            event.target.showModal();
-          }
-          break;
-        }
-        case "close": {
-          if (event.target.hasAttribute("open")) {
-            event.target.close();
-          }
-          break;
-        }
-      }
-      break;
-    }
-
-    case "video": {
-      switch (event.action) {
-        case "auto": {
-          if (event.target.paused) {
-            event.target.play();
-          } else {
-            event.target.pause();
-          }
-          break;
-        }
-        case "play": {
-          event.target.play();
-          break;
-        }
-        case "pause": {
-          event.target.pause();
-          break;
-        }
-        case "mute": {
-          if (event.target.volume === 0) {
-            event.target.volume = lastVolumes.get(event.target) || 1;
-          } else {
-            lastVolumes.set(event.target, event.target.volume);
-            event.target.volume = 0;
-          }
-          break;
-        }
-      }
-      break;
-    }
+  function teardownInvokeListeners(target) {
+    target.removeEventListener("click", handleInvokerActivation);
   }
-}
 
-function setupInvokeListeners(target) {
-  target.addEventListener("click", handleInvokerActivation);
-}
+  function observeShadowRoots(ElementClass, callback) {
+    const attachShadow = ElementClass.prototype.attachShadow;
+    ElementClass.prototype.attachShadow = function (init) {
+      const shadow = attachShadow.call(this, init);
+      callback(shadow);
+      return shadow;
+    };
+    const attachInternals = ElementClass.prototype.attachInternals;
+    ElementClass.prototype.attachInternals = function () {
+      const internals = attachInternals.call(this);
+      if (internals.shadowRoot) callback(internals.shadowRoot);
+      return internals;
+    };
+  }
 
-function teardownInvokeListeners(target) {
-  target.removeEventListener("click", handleInvokerActivation);
-}
-
-function observeShadowRoots(ElementClass, callback) {
-  const attachShadow = ElementClass.prototype.attachShadow;
-  ElementClass.prototype.attachShadow = function (init) {
-    const shadow = attachShadow.call(this, init);
-    callback(shadow);
-    return shadow;
-  };
-  const attachInternals = ElementClass.prototype.attachInternals;
-  ElementClass.prototype.attachInternals = function () {
-    const internals = attachInternals.call(this);
-    if (internals.shadowRoot) callback(internals.shadowRoot);
-    return internals;
-  };
-}
-
-export function apply() {
   const observer = new MutationObserver((mutations) => {
     for (const mutation of mutations) {
       if (mutation.type === "attributes") {
@@ -298,4 +256,10 @@ export function apply() {
   for (const invoker of document.querySelectorAll("[invoketarget]")) {
     setupInvokeListeners(invoker);
   }
-}
+
+  Object.defineProperty(window, "InvokeEvent", {
+    value: InvokeEvent,
+    configurable: true,
+    writable: true,
+  });
+})();
