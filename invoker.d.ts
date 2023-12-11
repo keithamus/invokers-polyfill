74557bc0292875899c91b8fdeaff3a2b8612d919
Keith Cirkel <keithamus@users.noreply.github.com>
2023-12-11 11:07:53 +0000

    fixup types

--- invoker.d.ts
+++ invoker.d.ts
@@ -3,25 +3,16 @@ interface InvokerMixin {
   invokeAction: string;
 }
 
-interface InterestMixin {
-  interestTargetElement: HTMLElement | null;
-}
-
 declare global {
   interface InvokeEvent extends Event {
-    relatedTarget: Element;
+    invoker: Element;
     action: string;
   }
-  interface InterestEvent extends Event {
-    relatedTarget: Element;
-  }
   /* eslint-disable @typescript-eslint/no-empty-interface */
-  interface HTMLButtonElement extends InvokerMixin, InterestMixin {}
-  interface HTMLInputElement extends InvokerMixin, InterestMixin {}
-  interface HTMLAnchorElement extends InterestMixin {}
+  interface HTMLButtonElement extends InvokerMixin {}
+  interface HTMLInputElement extends InvokerMixin {}
   /* eslint-enable @typescript-eslint/no-empty-interface */
   interface Window {
     InvokeEvent: InvokeEvent;
-    InterestEvent: InterestEvent;
   }
 }
